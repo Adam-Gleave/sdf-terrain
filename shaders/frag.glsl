822b83c24cded005c04da78c76241844289b5fdc
Adam-Gleave <adamg108@hotmail.co.uk>
2019-04-07 17:13:41 +0100

    Model/View matrix

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -8,13 +8,10 @@ uniform consts {
 
 out vec4 Target0;
 
-const int MAX_MARCHING_STEPS = 255;
-const float MIN_DIST = 0.0;
-const float MAX_DIST = 100.0;
 const float EPSILON = 0.0001;
 
 float f(float x, float z) {
-  return 0.0;
+  return (sin(x) - sin(z)) / 2.0;
 }
 
 vec3 rayDirection(float fieldOfView, vec2 size) {
@@ -26,16 +23,16 @@ vec3 rayDirection(float fieldOfView, vec2 size) {
 
 bool marchRay(vec3 origin, vec3 direction, inout float resT )
 {
-  const float dt = 0.01f;
-  const float mint = 0.001f;
-  const float maxt = 10.0f;
+  const float dt = 0.1;
+  const float mint = 0.001;
+  const float maxt = 100.0;
   
   for (float t = mint; t < maxt; t += dt)
   {
     const vec3 point = origin + direction * t;
-    if (point.y < f( point.x, point.z ))
+    if (point.y < f(point.x, point.z))
     {
-        resT = t - 0.5f*dt;
+        resT = t - 0.5 * dt;
         return true;
     }
   }
@@ -90,8 +87,8 @@ vec3 phongIllumination(
   const vec3 ambientLight = 0.025 * vec3(0.8, 0.8, 0.8);
   vec3 colour = ambient_colour * ambientLight;
 
-  vec3 lightPosition = vec3(2.0, 2.0, 2.0);
-  vec3 lightIntensity = vec3(0.4, 0.4, 0.4);
+  vec3 lightPosition = vec3(0.0, 20.0, 0.0);
+  vec3 lightIntensity = vec3(0.2, 0.4, 0.4);
 
   colour += phongAddForLight(
     diffuse_colour,
@@ -110,14 +107,32 @@ vec3 skyColour() {
   return vec3(0.6, 0.75, 1.0);
 }
 
+mat4 viewMatrix(vec3 eye, vec3 target, vec3 up) {
+  vec3 f = normalize(target - eye);
+  vec3 s = normalize(cross(f, up));
+  vec3 u = cross(s, f);
+
+  return mat4(
+    vec4(s, 0.0),
+    vec4(u, 0.0),
+    vec4(-f, 0.0),
+    vec4(0.0, 0.0, 0.0, 1)
+  );
+}
+
 void main() {
   vec3 dir = rayDirection(45.0, u_Resolution);
-  vec3 eye = vec3(0.0, 1.0, 5.0);
+  vec3 eye = vec3(40.0, 25.0, 35.0);
+  vec3 target = vec3(0.0, 0.0, 0.0);
+  vec3 up = vec3(0.0, 1.0, 0.0);
+
+  mat4 worldView = viewMatrix(eye, target, up);
+  vec3 worldDir = (worldView * vec4(dir, 0.0)).xyz;
 
   float t;
-  if (marchRay(eye, dir, t)) {
+  if (marchRay(eye, worldDir, t)) {
     // Lighting calculations
-    vec3 point = eye + dir * t;
+    vec3 point = eye + worldDir * t;
     
     vec3 ambient_colour = vec3(0.15, 0.4, 0.15);
     vec3 diffuse_colour = vec3(0.2, 0.8, 0.2);
